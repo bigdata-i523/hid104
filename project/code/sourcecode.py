642dfdb0f244328637d456fc9e776986bfb7c8c2
gabejone <31289927+gabejone@users.noreply.github.com>
2017-12-04 19:18:38 -0500

    Add files via upload

---
+++ project/code/sourcecode.py
@@ -0,0 +1,162 @@
+#Step 1: Import modules (some of visualizations will use custom imports later)
+import requests, json, csv
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+from math import pi
+from timeit import default_timer as timer
+
+
+
+#Step 2: Read Data from CSV and group into races
+file = open("Desktop/FA17/INFO-I423/Names_2010Census.csv", "r")
+name_csv = csv.DictReader(file)
+
+name_lst = [] #This list will be used later
+
+name_race = {'pctwhite':'', 'pctblack':'', 'pctapi':'', 'pcthispanic':''}
+
+for index, row in enumerate(name_csv):
+    if index >= 500:
+        break
+    name_lst.append(row['name'])
+    if max(row['pctwhite'], row['pctblack'], row['pctapi'], row['pctaian'], row['pct2prace'], row['pcthispanic']) == row['pctwhite']:
+        name_race['pctwhite']+=row['name']
+        name_race['pctwhite']+= ' '
+    elif max(row['pctwhite'], row['pctblack'], row['pctapi'], row['pctaian'], row['pct2prace'], row['pcthispanic']) == row['pctblack']:
+        name_race['pctblack']+=row['name']
+        name_race['pctblack']+= ' '
+    elif max(row['pctwhite'], row['pctblack'], row['pctapi'], row['pctaian'], row['pct2prace'], row['pcthispanic']) == row['pctapi']:
+        name_race['pctapi']+=row['name']
+        name_race['pctapi']+= ' '
+    elif max(row['pctwhite'], row['pctblack'], row['pctapi'], row['pctaian'], row['pct2prace'], row['pcthispanic']) == row['pcthispanic']:
+        name_race['pcthispanic']+=row['name']
+        name_race['pcthispanic']+= ' '
+
+file.close()
+
+names = {k: v + 1 for v, k in enumerate(name_lst)}
+
+name_race['pctwhite'] = name_race['pctwhite'].split()
+name_race['pctblack'] = name_race['pctblack'].split()
+name_race['pctapi'] = name_race['pctapi'].split()
+name_race['pcthispanic'] = name_race['pcthispanic'].split()
+
+name_counts = [len(name_race['pctwhite']), len(name_race['pctblack']), len(name_race['pctapi']), len(name_race['pcthispanic'])]
+
+#Step 3: Define function to capture search suggetion results
+
+#Define user agent set to Mozilla Firefox
+headers = {'User-agent':'Mozilla/5.0'}
+
+#Create a function to literate through names and capture search results
+def suggestion_master(results, dictionary, search, target, target2 = None):
+    for name in names.keys():
+        URL="http://suggestqueries.google.com/complete/search?client=firefox&q=" + name + " " + target
+        response = requests.get(URL, headers=headers)
+        results.append(json.loads(response.content.decode('utf-8')))    #captures search results and appends to a list
+    for result in results:  #iterates through search results for every name
+        name = result[0].split()
+        for suggestion in result[1]:
+            if target or target2 in suggestion:
+                dictionary[name[0]] += 1
+
+#Step 4 Run the test for search result case
+
+#Tracks how long it takes to run the functions
+start = timer()
+
+#4a
+arrest_results = []
+arrest_dict = {k: v*0 for v, k in enumerate(name_lst)}
+suggestion_master(arrest_results, arrest_dict, "arre", "arrest")
+
+#4b
+murder_results = []
+murder_dict = {k: v*0 for v, k in enumerate(name_lst)}
+suggestion_master(murder_results, murder_dict, "murd", "murder")
+
+#4c
+homicide_results = []
+homicide_dict = {k: v*0 for v, k in enumerate(name_lst)}
+suggestion_master(homicide_results, homicide_dict, "homi", "homicide")
+
+#4d
+crime_results = []
+crime_dict = {k: v*0 for v, k in enumerate(name_lst)}
+suggestion_master(crime_results, crime_dict, "crim", "crime", "criminal")
+
+#4e
+prison_results = []
+prison_dict = {k: v*0 for v, k in enumerate(name_lst)}
+suggestion_master(prison_results, prison_dict, "pris", "prison")
+
+#Tracks how long it takes to run the functions
+end = timer()
+query_time = (end - start)
+
+#Step 5 Summarise data results
+
+avg_data = [arrest_dict, murder_dict, homicide_dict, crime_dict, prison_dict]
+
+#5a Calculate search data results by race/ethnicity category
+
+pctwhite_stats = {'arrest':0, 'murder':0, 'homicide':0,'crime':0,'prison':0}
+
+pctblack_stats = {'arrest':0, 'murder':0, 'homicide':0,'crime':0,'prison':0}
+
+pctapi_stats = {'arrest':0, 'murder':0, 'homicide':0,'crime':0,'prison':0}
+
+pcthispanic_stats = {'arrest':0, 'murder':0, 'homicide':0,'crime':0,'prison':0}
+
+term_lst = ['arrest','murder','homicide','crime','prison']
+
+#Define a function to calculate results for every race/ethnicity and for every search criterion
+def stat_stuffer(race_name, pct_stats, avg_dict):
+    for i in range(0, len(avg_dict)):
+        for datum in avg_dict[i]:
+            for name in name_race[race_name]:
+                if datum == name:
+                    pct_stats[term_lst[i]]+=avg_dict[i][datum]
+
+
+#Run the function for every race/ethnicity
+stat_stuffer('pctwhite', pctwhite_stats, avg_data)
+
+stat_stuffer('pctblack', pctblack_stats, avg_data)
+
+stat_stuffer('pctapi', pctapi_stats, avg_data)
+
+stat_stuffer('pcthispanic', pcthispanic_stats, avg_data)
+
+
+#5b Average all the results
+
+#One dictionary stores just raw data which will be used later in the box plots
+name_race_raw = {'pctwhite':[], 'pctblack':[], 'pctapi':[], 'pcthispanic':[]}
+name_race_avg = {'pctwhite':0, 'pctblack':0, 'pctapi':0, 'pcthispanic':0}
+
+for race_name in name_race:
+    for data in avg_data:
+        for datum in data:
+                for name in name_race[race_name]:
+                    if datum == name:
+                        name_race_raw[race_name].append(data[datum])
+                        name_race_avg[race_name]+=data[datum]
+
+#Calculate average aggregate associations for every race/ethnicity
+for race_avg in name_race_avg:
+    name_race_avg[race_avg] = name_race_avg[race_avg] / (len(name_race[race_avg])*len(avg_data))
+
+#Calculate average associations for every race/ethnicity by search criteria
+for stat in pctwhite_stats:
+    pctwhite_stats[stat] = pctwhite_stats[stat] / len(name_race['pctwhite'])
+
+for stat in pctblack_stats:
+    pctblack_stats[stat] = pctblack_stats[stat] / len(name_race['pctblack'])
+
+for stat in pctapi_stats:
+    pctapi_stats[stat] = pctapi_stats[stat] / len(name_race['pctapi'])
+
+for stat in pcthispanic_stats:
+    pcthispanic_stats[stat] = pcthispanic_stats[stat] / len(name_race['pcthispanic'])
---
+++ project/code/visualizations.py
@@ -0,0 +1,187 @@
+#Step 6 Create Bar Chart Visualization
+
+import matplotlib.pyplot as plt; plt.rcdefaults()
+
+#Set up categories and data 
+objects = ('White ('+str(name_counts[0]) + ')', 'Black ('+str(name_counts[1]) + ')', 'Asian/Pacific ('+str(name_counts[2]) + ') ', 'Hispanic/Latino ('+str(name_counts[3]) + ')')
+y_pos = np.arange(len(objects))
+performance = [name_race_avg['pctwhite'],name_race_avg['pctblack'],name_race_avg['pctapi'],name_race_avg['pcthispanic']]
+
+#Plot data
+plt.bar(y_pos, performance, align='center', alpha=2)
+plt.xticks(y_pos, objects)
+plt.ylabel('Avg Association')
+plt.title('Nevative Word Associations by Race/Ethnicity')
+
+fig1 = plt.figure(1, figsize=(9, 6))
+
+fig1.savefig('fig1.png', bbox_inches='tight')
+
+plt.show()
+
+#Step 7 Create Box Plot Visualizations
+
+import matplotlib as mpl
+mpl.use('agg')
+
+## Create data
+collectn_1 = name_race_raw['pctwhite']
+collectn_2 = name_race_raw['pctblack']
+collectn_3 = name_race_raw['pctapi']
+collectn_4 = name_race_raw['pcthispanic']
+
+## combine these different collections into a list    
+data_to_plot = [collectn_1, collectn_2, collectn_3, collectn_4]
+
+# Create a figure instance
+fig2 = plt.figure(2, figsize=(9, 6))
+
+# Create an axes instance
+ax = fig2.add_subplot(111)
+
+# Create the boxplot
+bp = ax.boxplot(data_to_plot)
+
+## add patch_artist=True option to ax.boxplot() 
+## to get fill color
+bp = ax.boxplot(data_to_plot, patch_artist=True)
+
+## change outline color, fill color and linewidth of the boxes
+for box in bp['boxes']:
+    # change outline color
+    box.set( color='#7570b3', linewidth=2)
+    # change fill color
+    box.set( facecolor = '#1b9e77' )
+
+## change color and linewidth of the whiskers
+for whisker in bp['whiskers']:
+    whisker.set(color='#7570b3', linewidth=2)
+
+## change color and linewidth of the caps
+for cap in bp['caps']:
+    cap.set(color='#7570b3', linewidth=2)
+
+## change color and linewidth of the medians
+for median in bp['medians']:
+    median.set(color='#b2df8a', linewidth=2)
+
+## change the style of fliers and their fill
+for flier in bp['fliers']:
+    flier.set(marker='o', color='#e7298a', alpha=0.5)
+
+## Custom x-axis labels
+ax.set_xticklabels(['White ('+str(name_counts[0]) + ')', 'Black ('+str(name_counts[1]) + ')', 'Asian/Pacific ('+str(name_counts[2]) + ')', 'Hispanic/Latino ('+str(name_counts[3]) + ')'])
+
+## Remove top axes and right axes ticks
+ax.get_xaxis().tick_bottom()
+ax.get_yaxis().tick_left()
+
+plt.title('Nevative Word Associations by Race/Ethnicity')
+
+# Save the figure
+fig2.savefig('fig2.png', bbox_inches='tight')
+
+plt.show()
+
+
+#Step 8 Create Multiple Radar Visualizations
+# Set data frame
+df = pd.DataFrame({
+'group': ['White ('+str(name_counts[0]) + ')', 'Black ('+str(name_counts[1]) + ')', 'Asian/Pacific ('+str(name_counts[2]) + ')', 'Hispanic/Latino ('+str(name_counts[3]) + ')'],
+'arrest': [pctwhite_stats['arrest'], pctblack_stats['arrest'], pctapi_stats['arrest'], pcthispanic_stats['arrest']],
+'murder': [pctwhite_stats['murder'], pctblack_stats['murder'], pctapi_stats['murder'], pcthispanic_stats['murder']],
+'homicide': [pctwhite_stats['homicide'], pctblack_stats['homicide'], pctapi_stats['homicide'], pcthispanic_stats['homicide']],
+'crime': [pctwhite_stats['crime'], pctblack_stats['crime'], pctapi_stats['crime'], pcthispanic_stats['crime']],
+'prison': [pctwhite_stats['prison'], pctblack_stats['prison'], pctapi_stats['prison'], pcthispanic_stats['prison']]
+})
+
+# number of variables
+categories=list(df)[:2]
+categories+=list(df)[3:]
+N = len(categories)
+
+def make_spider(row, title, color):
+     
+    # What will be the angle of each axis in the plot? (we divide the plot / number of variable)
+    angles = [n / float(N) * 2 * pi for n in range(N)]
+    angles += angles[:1]
+     
+    # Initialise the spider plot
+    ax = plt.subplot(2,2,row+1, polar=True, )
+     
+    # If you want the first axis to be on top:
+    ax.set_theta_offset(pi / 2)
+    ax.set_theta_direction(-1)
+     
+    # Draw one axe per variable + add labels labels yet
+    plt.xticks(angles[:-1], categories, color='grey', size=8)
+     
+    # Draw ylabels
+    ax.set_rlabel_position(0)
+    plt.yticks([2,4,6,8], ["2","4","6","8"], color="grey", size=7)
+    plt.ylim(0,10)
+     
+    # Ind1
+    values=df.loc[row].drop('group').values.flatten().tolist()
+    values += values[:1]
+    ax.plot(angles, values, color=color, linewidth=2, linestyle='solid')
+    ax.fill(angles, values, color=color, alpha=0.4)
+     
+    # Add a title
+    plt.title(title, size=11, color=color, y=1.1)
+
+# ------- PART 2: Apply to all individuals
+# initialize the figure
+my_dpi=96
+fig3 = plt.figure(figsize=(1000/my_dpi, 1100/my_dpi), dpi=my_dpi)
+ 
+# Create a color palette:
+my_palette = plt.cm.get_cmap("Set2", len(df.index))
+ 
+# Loop to plot
+for row in range(0, len(df.index)):
+    make_spider( row=row, title=df['group'][row], color=my_palette(row))
+
+fig3.savefig('fig3.png', bbox_inches='tight')
+
+plt.show()
+
+#Step 9 Create Joint Radar Visualization
+
+# What will be the angle of each axis in the plot? (we divide the plot / number of variable)
+angles = [n / float(N) * 2 * pi for n in range(N)]
+angles += angles[:1]
+ 
+# Initialise the spider plot
+ax = plt.subplot(111, polar=True)
+ 
+# If you want the first axis to be on top:
+ax.set_theta_offset(pi / 2)
+ax.set_theta_direction(-1)
+ 
+# Draw one axe per variable + add labels labels yet
+plt.xticks(angles[:-1], categories)
+ 
+# Draw ylabels
+ax.set_rlabel_position(0)
+plt.yticks([2,4,6,8], ["2","4","6","8"], color="grey", size=7)
+plt.ylim(0,10)
+
+
+ 
+# ------- PART 2: Add plots
+ 
+# Plot each individual = each line of the data
+# I don't do a loop, because plotting more than 3 groups makes the chart unreadable
+
+my_palette = plt.cm.get_cmap("Set2", len(df.index))
+    
+for i in range(0,len(df.index)):
+    values=df.loc[i].drop('group').values.flatten().tolist()
+    values += values[:1]
+    ax.plot(angles, values, color=my_palette(i), linewidth=1, linestyle='solid', label="group A")
+    ax.fill(angles, values, color=my_palette(i), alpha=0.1)
+
+plt.savefig('fig4.png')
+
+plt.show()
